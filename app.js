2acd0e0c7383d82a8d7a984f31428269c32b9277
aarondavidgedev <aarondavidgeoptimusprime@gmail.com>
2022-04-14 21:31:56 -0500

    fetches data and displys in div

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
-alert("Welcome to Aaron's Coding Challenege.");
 
 function fetchdata(){
-    console.log("Starting fetch function.")
-    fetch("https://my-json-server.typicode.com/aarondavidgedev/codingchallenege/posts").then(response => {
-        console.log(response);
+    
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+        if (!response.ok){
+            throw Error("ERROR");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data)
+        const html = data
+        .map(user => {
+            return `
+            <div class="users">
+            <p>${user.id}</p>
+            <p>${user.name}</p>
+            <p>${user.username}</p>
+            <p>${user.email}</p>
+            <p>${user.address.street}</p>
+            <p>${user.address.suite}</p>
+            <p>${user.address.city}</p>
+            <p>${user.address.zipcode}</p>
+            <p>${user.address.geo.lat}</p>
+            <p>${user.address.geo.lng}</p>
+            <p>${user.phone}</p>
+            <p>${user.company.name}</p>
+            <p>${user.company.catchPhrase}</p>
+            <p>${user.company.bs}</p>
+            </div>
+            `;
+        })
+        .join("");
+        console.log(html);
+        document.querySelector("#Table").insertAdjacentHTML("afterbegin",html);
+        
+        })
+        
+    .catch(error => {
+        console.log(error);
     });
 }
 
